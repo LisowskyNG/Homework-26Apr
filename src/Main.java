d06582d02fba524c72eb9fc95f03123411797722
Valery Maltsev <lisowsky@yandex.ru>
2023-05-09 01:05:54 +0200

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+
     }
+    public static void task1() {
+        System.out.println("Задача 1");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        for (Integer num : nums) {
+            if (num % 2 != 0) {
+                System.out.print(num + ", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        // Пишем код для задачи 2
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        // Пишем код для задачи 3
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        // Пишем код для задачи 4
+
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
+        // Пишем код для задачи 5
+
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        // Пишем код для задачи 6
+
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        // Пишем код для задачи 7
+
+    }
+    public static void task8 () {
+        System.out.println("Задача 8");
+        // Пишем код для задачи 8
+
+    }
+
 }
+
